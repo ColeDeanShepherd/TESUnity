98ef39fe3efaa739bc1c489fafc5c807241888ad
ColeDeanShepherd <cole@coledeanshepherd.com>
2016-05-09 02:27:19 -0500

    Still trying to fix the bug.

--- Assets/Scripts/BinaryReaderExtensions.cs
+++ Assets/Scripts/BinaryReaderExtensions.cs
@@ -18,58 +18,128 @@ public static class BinaryReaderExtensions
 
 	public static string ReadASCIIString(BinaryReader reader, int length)
 	{
-		return System.Text.Encoding.ASCII.GetString(reader.ReadBytes(length));
+		Debug.Assert(length >= 0);
+
+		return Encoding.ASCII.GetString(reader.ReadBytes(length));
 	}
 	public static string ReadPossiblyNullTerminatedASCIIString(BinaryReader reader, int lengthIncludingPossibleNullTerminator)
 	{
+		Debug.Assert(lengthIncludingPossibleNullTerminator > 0);
+
 		var bytes = reader.ReadBytes(lengthIncludingPossibleNullTerminator);
 		var charCount = (Utils.Last(bytes) != 0) ? bytes.Length : (bytes.Length - 1);
 
-		return System.Text.Encoding.ASCII.GetString(bytes, 0, charCount);
+		return Encoding.ASCII.GetString(bytes, 0, charCount);
 	}
 	public static string ReadLength32PrefixedASCIIString(BinaryReader reader)
 	{
-		return System.Text.Encoding.ASCII.GetString(ReadLength32PrefixedBytes(reader));
+		return Encoding.ASCII.GetString(ReadLength32PrefixedBytes(reader));
 	}
 
+	public static Vector2 ReadVector2(BinaryReader reader)
+	{
+		var x = reader.ReadSingle();
+		var y = reader.ReadSingle();
+
+		return new Vector2(x, y);
+	}
 	public static Vector3 ReadVector3(BinaryReader reader)
 	{
-		float x = reader.ReadSingle();
-		float y = reader.ReadSingle();
-		float z = reader.ReadSingle();
+		var x = reader.ReadSingle();
+		var y = reader.ReadSingle();
+		var z = reader.ReadSingle();
 
 		return new Vector3(x, y, z);
 	}
 	public static Vector4 ReadVector4(BinaryReader reader)
 	{
-		float x = reader.ReadSingle();
-		float y = reader.ReadSingle();
-		float z = reader.ReadSingle();
-		float w = reader.ReadSingle();
+		var x = reader.ReadSingle();
+		var y = reader.ReadSingle();
+		var z = reader.ReadSingle();
+		var w = reader.ReadSingle();
 
 		return new Vector4(x, y, z, w);
 	}
 
+	/// <summary>
+	/// Reads a column-major 3x3 matrix but returns a functionally equivalent 4x4 matrix.
+	/// </summary>
 	public static Matrix4x4 ReadColumnMajorMatrix3x3(BinaryReader reader)
 	{
-		var mat = new Matrix4x4();
+		var matrix = new Matrix4x4();
 
-		for(int j = 0; j < 4; j++)
+		for(int columnIndex = 0; columnIndex < 4; columnIndex++)
 		{
-			for(int i = 0; i < 4; i++)
+			for(int rowIndex = 0; rowIndex < 4; rowIndex++)
 			{
-				if(i < 3 && j < 3)
+				// If we're in the 3x3 part of the matrix, read values from the BinaryReader. Otherwise, use the identity matrix.
+				if(rowIndex <= 2 && columnIndex <= 2)
 				{
-					mat[i, j] = reader.ReadSingle();
+					matrix[rowIndex, columnIndex] = reader.ReadSingle();
 				}
 				else
 				{
-					mat[i, j] = (i == j) ? 1 : 0;
+					matrix[rowIndex, columnIndex] = (rowIndex == columnIndex) ? 1 : 0;
 				}
 			}
 		}
 
-		return mat;
+		return matrix;
+	}
+
+	/// <summary>
+	/// Reads a row-major 3x3 matrix but returns a functionally equivalent 4x4 matrix.
+	/// </summary>
+	public static Matrix4x4 ReadRowMajorMatrix3x3(BinaryReader reader)
+	{
+		var matrix = new Matrix4x4();
+
+		for(int rowIndex = 0; rowIndex < 4; rowIndex++) 
+		{
+			for(int columnIndex = 0; columnIndex < 4; columnIndex++)
+			{
+				// If we're in the 3x3 part of the matrix, read values from the BinaryReader. Otherwise, use the identity matrix.
+				if(rowIndex <= 2 && columnIndex <= 2)
+				{
+					matrix[rowIndex, columnIndex] = reader.ReadSingle();
+				}
+				else
+				{
+					matrix[rowIndex, columnIndex] = (rowIndex == columnIndex) ? 1 : 0;
+				}
+			}
+		}
+
+		return matrix;
+	}
+
+	public static Matrix4x4 ReadColumnMajorMatrix4x4(BinaryReader reader)
+	{
+		var matrix = new Matrix4x4();
+
+		for(int columnIndex = 0; columnIndex < 4; columnIndex++)
+		{
+			for(int rowIndex = 0; rowIndex < 4; rowIndex++)
+			{
+				matrix[rowIndex, columnIndex] = reader.ReadSingle();
+			}
+		}
+
+		return matrix;
+	}
+	public static Matrix4x4 ReadRowMajorMatrix4x4(BinaryReader reader)
+	{
+		var matrix = new Matrix4x4();
+
+		for(int rowIndex = 0; rowIndex < 4; rowIndex++) 
+		{
+			for(int columnIndex = 0; columnIndex < 4; columnIndex++)
+			{
+				matrix[rowIndex, columnIndex] = reader.ReadSingle();
+			}
+		}
+
+		return matrix;
 	}
 
 	public static Quaternion ReadQuaternionWFirst(BinaryReader reader)
@@ -78,6 +148,15 @@ public static class BinaryReaderExtensions
 		float x = reader.ReadSingle();
 		float y = reader.ReadSingle();
 		float z = reader.ReadSingle();
+
+		return new Quaternion(x, y, z, w);
+	}
+	public static Quaternion ReadQuaternionWLast(BinaryReader reader)
+	{
+		float x = reader.ReadSingle();
+		float y = reader.ReadSingle();
+		float z = reader.ReadSingle();
+		float w = reader.ReadSingle();
 
 		return new Quaternion(x, y, z, w);
 	}
