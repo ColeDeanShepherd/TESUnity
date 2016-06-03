ffe9ffe4ec7599318f9b696109a496be9b876e27
Thomas Rasor <thomas.ir.rasor@gmail.com>
2016-06-03 18:53:00 -0500

    Added a toggle to disable the MusicPlayer on scene start. MP3 streaming has a large impact on performance at certain intervals and should probably be avoided without a better more performant solution in the future.

--- Assets/Scripts/TES/TESUnity.cs
+++ Assets/Scripts/TES/TESUnity.cs
@@ -10,6 +10,8 @@ namespace TESUnity
 		public static TESUnity instance;
 
 		#region Inspector-set Members
+		public bool music = false;
+
 		public Sprite UIBackgroundImg;
 		public Sprite UICheckmarkImg;
 		public Sprite UIDropdownArrowImg;
@@ -43,18 +45,20 @@ namespace TESUnity
 			MWDataReader = new MorrowindDataReader(MWDataPath);
 			MWEngine = new MorrowindEngine(MWDataReader);
 
-			// Start the music.
-			musicPlayer = new MusicPlayer();
+			if ( music )
+			{// Start the music.
+				musicPlayer = new MusicPlayer();
 
-			foreach(var songFilePath in Directory.GetFiles(MWDataPath + "/Music/Explore"))
-			{
-				if(!songFilePath.Contains("Morrowind Title"))
+				foreach ( var songFilePath in Directory.GetFiles( MWDataPath + "/Music/Explore" ) )
 				{
-					musicPlayer.AddSong(songFilePath);
+					if ( !songFilePath.Contains( "Morrowind Title" ) )
+					{
+						musicPlayer.AddSong( songFilePath );
+					}
 				}
-			}
 
-			musicPlayer.Play();
+				musicPlayer.Play();
+			}
 
 			// Spawn the player.
 			MWEngine.SpawnPlayerInside("Imperial Prison Ship", new Vector3(0.8f, -0.25f, -1.4f));
@@ -71,7 +75,7 @@ namespace TESUnity
 		private void Update()
 		{
 			MWEngine.Update();
-			musicPlayer.Update();
+			if ( music ) musicPlayer.Update();
 
 			if(Input.GetKeyDown(KeyCode.P))
 			{
