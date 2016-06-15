4e4d26e90c0c5abf8b5d8a852a88c9c5265a54ae
ColeDeanShepherd <cole@coledeanshepherd.com>
2016-06-14 20:41:34 -0500

    Refactored Vector2i.

--- Assets/Scripts/Vector2i.cs
+++ Assets/Scripts/Vector2i.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
+/// <summary>
+/// A 2-dimensional vector with integer components.
+/// </summary>
 public struct Vector2i : IEquatable<Vector2i>
 {
-	public static Vector2i zero
-	{
-		get
-		{
-			return new Vector2i(0, 0);
-		}
-	}
+	public static Vector2i zero { get { return new Vector2i(0, 0); } }
+	public static Vector2i right { get { return new Vector2i(1, 0); } }
+	public static Vector2i up { get { return new Vector2i(0, 1); } }
+	public static Vector2i one { get { return new Vector2i(1, 1); } }
 
 	public int x, y;
 
