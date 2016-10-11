4dc76eb642d8d39405816d39a90e4b78d80b67ac
Yannick <comte.yannick@gmail.com>
2016-10-11 08:05:18 +0200

    Working on the book system

--- Assets/Scripts/TES/Components/PlayerInventory.cs
+++ Assets/Scripts/TES/Components/PlayerInventory.cs
@@ -9,12 +9,14 @@ namespace TESUnity.Components
     {
         private List<Record> _inventory = new List<Record>();
         private Transform _disabledObjects = null;
+        private PlayerComponent _player = null;
 
         void Start()
         {
             var disabledObjectGO = new GameObject("DisabledObjects");
             disabledObjectGO.SetActive(false);
             _disabledObjects = disabledObjectGO.GetComponent<Transform>();
+            _player = GetComponent<PlayerComponent>();
         }
 
         public void Add(GenericObjectComponent item)
@@ -22,27 +24,15 @@ namespace TESUnity.Components
             Add(item.record);
 
             // For now.
-            var weapon = item.record as WEAPRecord;
+            var weapon = item as WeaponComponent;
             if (weapon != null)
             {
-                //var p = obj.transform;
-                //while (p.parent != null && p.parent.gameObject.name != "objects")
-                    //p = p.parent;
+                var rightHand = _player.rightHand;
+                if (rightHand.childCount > 0)
+                    rightHand.GetChild(0).parent = _disabledObjects;
 
-                var renderer = item.GetComponentInChildren<Renderer>();
-                if (renderer != null)
-                {
-                    var rightHand = GetComponent<PlayerComponent>().rightHand;
-                    if (rightHand.childCount > 0)
-                        rightHand.GetChild(0).parent = _disabledObjects;
-
-                    ((WeaponComponent)item).Equip(rightHand.transform);
-                    /*var target = renderer.transform.parent;
-                    target.parent = rightHand.transform;
-                    target.localPosition = Vector3.zero;
-                    target.localRotation = Quaternion.identity;*/
-                    return;
-                }
+                ((WeaponComponent)item).Equip(rightHand);
+                return;
             }
 
             item.transform.parent = _disabledObjects.transform;
--- Assets/Scripts/TES/UI/UIBook.cs
+++ Assets/Scripts/TES/UI/UIBook.cs
@@ -6,9 +6,14 @@ namespace TESUnity.UI
     public class UIBook : MonoBehaviour
     {
         private static Sprite BookSprite;
+        private int _numberOfPages;
+        private int _currentPage;
+        private string[] _pages;
 
         [SerializeField]
         private GameObject _container = null;
+        [SerializeField]
+        private Image _background = null;
         [SerializeField]
         private Text _content1 = null;
         [SerializeField]
@@ -26,14 +31,28 @@ namespace TESUnity.UI
                 BookSprite = GUIUtils.CreateSprite(texture);
             }
 
-            _container.GetComponent<Image>().sprite =BookSprite;
+            _background.sprite = BookSprite;
         }
 
-        public void Show(string[] pages)
+        public void Show(string pages)
         {
+            // 1. Replace <BR> by jump
+            // 2. Remove HTML tags
+            // 3. Determine the number of pages
+            // 4. Display the first and second page
+            // 5. Update buttons.
+            
+            pages = pages.Replace("<BR>", "\r\n");
+            pages = System.Text.RegularExpressions.Regex.Replace(pages, @"<[^>]*>", string.Empty);
+
+            var countChar = pages.Length;
+            var numCharPerPage = 350;
+            _numberOfPages = Mathf.CeilToInt(countChar / numCharPerPage);
+            _currentPage = 0;
+            _pages = new string[_numberOfPages];
 
         }
-        
+
         public void Take()
         {
             Debug.Log("Take");
